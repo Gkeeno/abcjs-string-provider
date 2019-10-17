6344c2fa5430744adbcb5e6dd25be22c7256985d
派小星 <brian_tr@163.com>
2019-10-17 14:14:02 +0800

    add

---
+++ src/views/canvas/index.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="canvasIndex">
+    <canvas id="c" width="800" height="800"></canvas>
+    <div class="box">
+      <div class="btn_group">
+        <button class="btn" @click="add('arrow')">箭头</button>
+        <button class="btn" @click="add('collect')">收音符</button>
+      </div>
+      <div class="text">
+        <input type="text" v-model="someText" />
+        <button class="addText" @click="addText">添加文字</button>
+      </div>
+      <button class="save" @click="save">保存</button>
+      <button class="read" @click="read">读取</button>
+      <input type="text" @keyup.delete="remove" ref="removeBox" />
+    </div>
+    <canvas id="b" width="600" height="600"></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+import { fabric } from 'fabric';
+import { Component, Prop, Vue, Provide } from 'vue-property-decorator';
+@Component
+export default class canvasIndex extends Vue {
+  public _canvas;
+  public checked: Boolean = false;
+  public someText: string = '';
+  public jsonData: string = '';
+  public selectId: number = 0;
+  public selectObj;
+  public mounted() {
+    const _this = this;
+    fabric.Object.prototype.noScaleCache = false;
+    /*
+	strokeUniform works better without scalingCache
+	Objects in group are not scaled directly, so stroke uniform will not have effect.
+*/
+
+    const canvas = (this._canvas = new fabric.Canvas('c'));
+
+    // create a rectangle object
+    const rect = new fabric.Rect({
+      id: 1,
+      left: 150,
+      top: 200,
+      originX: 'left',
+      originY: 'top',
+      width: 150,
+      height: 60,
+      angle: -10,
+      fill: 'rgba(255,0,0,0.5)',
+      transparentCorners: false,
+      borderColor: 'blue',
+      allowTouchScrolling: false
+    });
+    const path = new fabric.Path('M 0 0 L 200 100 L 170 200 z');
+    path.set({ left: 320, top: 220, fill: 'red' });
+    canvas.add(path).setActiveObject(path);
+
+    canvas.add(rect).setActiveObject(rect);
+    canvas.item(0).hasControls = true;
+    canvas.item(0).selectable = true;
+    canvas.on('mouse:down', options => {});
+    rect.on('moving', function(options) {
+      // console.log(options);
+    });
+    rect.on('selected', options => {
+      this.selectObj = rect;
+      _this.$refs.removeBox.focus();
+    });
+    const itemArr = canvas.getObjects();
+    console.log(itemArr);
+  }
+  public add(type) {
+    let imgUrl = '';
+    if (type === 'arrow') {
+      imgUrl = require('../../assets/canvasimg/arrow.png');
+    } else {
+      imgUrl = require('../../assets/canvasimg/collectNotes.png');
+    }
+    fabric.Image.fromURL(imgUrl, img => {
+      const oImg = img
+        .set({
+          width: 400,
+          height: 400,
+          originX: 'left',
+          originY: 'top',
+          left: 20,
+          top: 20
+        })
+        .scale(0.25);
+      this._canvas.add(oImg);
+    });
+  }
+  public addText() {
+    if (this.someText) {
+      const text = new fabric.IText(this.someText, {
+        // 绘制文本
+        fontSize: 30,
+        originX: 'center',
+        originY: 'center',
+        width: 150,
+        left: 30,
+        top: 40,
+        fill: '#D81B60'
+      });
+      this._canvas.add(text).setActiveObject(text);
+      this.someText = '';
+    }
+  }
+  public save() {
+    const canvas = this._canvas;
+    if (!this.jsonData) {
+      this.jsonData = JSON.stringify(canvas.toJSON());
+      // console.log(canvas.toDataURL('png'));
+    }
+    // console.log(typeof JSON.stringify(canvas.toJSON()));
+  }
+  public read() {
+    if (this.jsonData) {
+      const canvas = new fabric.Canvas('b');
+      // const canvas = this._canvas;
+      // console.log(canvas.loadFromJSON(this.jsonData));
+      canvas.loadFromJSON(this.jsonData);
+    }
+  }
+  public remove() {
+    const itemArr = this._canvas.getObjects();
+    this._canvas.remove(this.selectObj);
+    // const findItem = itemArr.find(item => {
+    //   return;
+    // });
+  }
+}
+</script>
+
+<style lang="less" scoped>
+#c {
+  border: 1px solid #ff7200;
+}
+#b {
+  border: 1px solid blue;
+}
+.canvasIndex {
+  display: flex;
+}
+</style>
