588b29c563c8dce55bf416e140a0c330f0255ccc
xi.zhang <xi.zhang@digilinkcn.com>
2020-02-04 22:31:30 +0800

    changed .gitignore去除a-r-e发布地文件

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import SingleTrack from './views/SingleTrack.vue';
 import DoubleTrack from './views/DoubleTrack.vue';
 
 Vue.use(Router);
@@ -10,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: SingleTrack
     },
     {
       path: '/double-track',
