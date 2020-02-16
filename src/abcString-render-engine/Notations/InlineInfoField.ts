20df9f24b89fc8e1512cb09fd1505673bb935079
xi.zhang <xi.zhang@digilinkcn.com>
2020-02-16 21:00:50 +0800

    fixed inlineField序列化错误

--- src/abcString-render-engine/Notations/InlineInfoField.ts
+++ src/abcString-render-engine/Notations/InlineInfoField.ts
@@ -1,9 +1,14 @@
 import { InfoField } from './InfoField';
 import { NotationType } from '../Enums/NotationType';
+import { NotationSerializeInfo } from '../common';
 
 export class InlineInfoField extends InfoField {
   ntype = NotationType.IninlineInfoField;
 
+  public static deserialize(seriInfo: NotationSerializeInfo) {
+    return new InlineInfoField(...seriInfo.state)
+  }
+  
   public toAbcString() {
     return `[${this.fieldType + this.content}]`;
   }
